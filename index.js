e6c52cd5472ac54062d64fd2ea31984a545d7351
jedidiah <jedaqsaul@gmail.com>
2025-03-13 10:04:15 +0300

    Completed the phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(3, 4, 5, 20);
+console.log(max);
